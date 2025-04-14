c060c58ef3ddf9b359203d537fd058e396d10632
Joshua. C <76528955+jemcap@users.noreply.github.com>
2025-04-14 15:10:31 +0100

    Implemented login and authorisation functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,7 @@
 import express from "express";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
-
+import bcrypt from "bcrypt";
 const authRoutes = express.Router();
 
 authRoutes.post("/register", async (req, res, next) => {
@@ -19,6 +18,22 @@ authRoutes.post("/register", async (req, res, next) => {
 
     await user.save();
 
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw Error;
+        console.log(token);
+        res.json({ token });
+      }
+    );
     res.status(201).json({ message: "User created", data: user });
   } catch (error) {
     console.log(error.message);
@@ -33,9 +48,33 @@ authRoutes.post("/login", async (req, res) => {
     if (!email || !password) return;
     let user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const token = jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+      (err, token) => {
+        if (err) throw Error;
+        res.json({ token });
+      }
+    );
+    res.status(200).json({ message: "User has successfully logged in", token });
   } catch (error) {
     console.log(error.message);
     res.status(404).json({ message: error.message });
+    next();
   }
 });
 
